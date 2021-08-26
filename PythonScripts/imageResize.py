a2f5f78ec97e6cd0d3b0a88f5d0dcf4e1ea4512b
Kamden-Brothers <89610482+Kamden-Brothers@users.noreply.github.com>
2021-08-26 17:00:28 -0600

    Update imageResize.py

--- PythonScripts/imageResize.py
+++ PythonScripts/imageResize.py
@@ -2,14 +2,9 @@ from PIL import Image
 from os import listdir
 import os
 from os.path import isfile, join
-mypath = r"C:\Users\kamde\Pictures\CarImages"
+mypath = r"C:\Users\kamde\Pictures\CarImages"                               #Folder with images that need to be resized.
 
-try:
-    os.mkdir(mypath + r"\lowerRes")
-except OSError as error:
-    print("directory exists")
-    
-allFiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+allFiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]          #this finds all files within a given directory
 
 imgFiles = []
 
@@ -17,21 +12,20 @@ for oneFile in allFiles:
     length = len(oneFile)
     print(length)
     print(oneFile[(length-3):(length)])
-    if(oneFile[(length-3):(length)] == "JPG"):
-       imgFiles.append(oneFile)
+    if(oneFile[(length-3):(length)] == "JPG"):          #checks for all JPG within the folder
+       imgFiles.append(oneFile)                         
 
 
-for imgFile in imgFiles:
+for imgFile in imgFiles:        #resizes each image
 
     im = Image.open(mypath + "/" + imgFile);
 
-    newWidth = 700;
+    newWidth = 700;                                     #new width dimension
     width = im.size[0];
     height = im.size[1];
 
-    wpercent = (newWidth/float(width))
-    newHeight = int(float(height)*float(wpercent));
+    wpercent = (newWidth/float(width))                  #finds the ratio between new width and the old width
+    newHeight = int(float(height)*float(wpercent));     #multiplies height by that ration
 
     im = im.resize((newWidth,newHeight), Image.ANTIALIAS);
-    #im.show();
-    im.save(mypath + imgFile);
+    im.save(mypath + imgFile);                  #could create a new path to save if you want to save the original images
