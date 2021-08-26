0e69c59356272898121a1130a7af8e55a40b9645
Kamden-Brothers <89610482+Kamden-Brothers@users.noreply.github.com>
2021-08-26 16:39:05 -0600

    Add files via upload

--- PythonScripts/imageResize.py
+++ PythonScripts/imageResize.py
@@ -1,13 +1,37 @@
 from PIL import Image
+from os import listdir
+import os
+from os.path import isfile, join
+mypath = r"C:\Users\kamde\Pictures\CarImages"
 
-im = Image.open(r"C:\Users\kamde\Pictures\CarImages\IMG_1776.JPG");
+try:
+    os.mkdir(mypath + r"\lowerRes")
+except OSError as error:
+    print("directory exists")
+    
+allFiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-newWidth = 700;
-width = im.size[0];
-height = im.size[1];
+imgFiles = []
 
-wpercent = (newWidth/float(width))
-newHeight = int(float(height)*float(wpercent));
+for oneFile in allFiles:
+    length = len(oneFile)
+    print(length)
+    print(oneFile[(length-3):(length)])
+    if(oneFile[(length-3):(length)] == "JPG"):
+       imgFiles.append(oneFile)
 
-img = im.resize((newWidth,newHeight), Image.ANTIALIAS);
-img.show();
+
+for imgFile in imgFiles:
+
+    im = Image.open(mypath + "/" + imgFile);
+
+    newWidth = 700;
+    width = im.size[0];
+    height = im.size[1];
+
+    wpercent = (newWidth/float(width))
+    newHeight = int(float(height)*float(wpercent));
+
+    im = im.resize((newWidth,newHeight), Image.ANTIALIAS);
+    #im.show();
+    im.save(mypath + imgFile);
