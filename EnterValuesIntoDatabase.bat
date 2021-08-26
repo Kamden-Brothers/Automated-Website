f1a0ece741d287ffd4677cca74f7637ee7713599
Kamden-Brothers <89610482+Kamden-Brothers@users.noreply.github.com>
2021-08-26 15:10:59 -0600

    Add files via upload

---
+++ EnterValuesIntoDatabase.bat
@@ -0,0 +1,5 @@
+python "C:\Users\kamde\nodejsPractice\WriteHtmlCode.py"
+
+python "C:\Users\kamde\nodejsPractice\photoFinder.py"
+
+psql -U postgres -f "C:\Program Files\PostgreSQL\13\ServerUpdate.txt"
